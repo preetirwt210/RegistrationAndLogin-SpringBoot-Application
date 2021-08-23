47ba97af6ffd0611a11f7658679e91b27c0427b3
Preeti Rawat <preetirawat711@gmail.com>
2021-08-23 19:00:19 +0530

    RegistrationAndLogin-SpringBoot-Application

---
+++ MyApplication/src/test/java/com/newproject/springboot/myApp/StudentFeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.newproject.springboot.myApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentFeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
